7fc2e2ec450d349f20e1b4ef600b6902e09e2880
shienamarie1 <shienamariecabiling119@gmail.com>
2026-05-10 08:30:00 +0800

    Fixing add-Item

--- account/routers/add-item.php
+++ account/routers/add-item.php
@@ -3,8 +3,9 @@ include '../includes/connect.php';
 
 $name = $_POST['name'];
 $price = $_POST['price'];
+$stock = isset($_POST['stock']) ? intval($_POST['stock']) : 0;
 $image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
-$sql = "INSERT INTO items (name, price,image) VALUES ('$name', $price,'$image')";
+$sql = "INSERT INTO items (name, price, stock, image) VALUES ('$name', $price, $stock, '$image')";
 $con->query($sql);
 if(!$con)
 {
