ffdd5de56cb760cb50ed292018c16d7f7cae639d
shienamarie1 <shienamariecabiling119@gmail.com>
2026-05-10 08:30:00 +0800

    Fixing Menu_Router

--- account/routers/menu-router.php
+++ account/routers/menu-router.php
@@ -1,37 +1,90 @@
 <?php
 include '../includes/connect.php';
-	foreach ($_POST as $key => $value)
-	{
-		if(preg_match("/[0-9]+_name/",$key)){
-			if($value != ''){
-			$key = strtok($key, '_');
-			$value = htmlspecialchars($value);
-			$sql = "UPDATE items SET name = '$value' WHERE id = $key;";
-			$con->query($sql);
-			}
-		}
-		if(preg_match("/[0-9]+_price/",$key)){
-			$key = strtok($key, '_');
-			$sql = "UPDATE items SET price = $value WHERE id = $key;";
-			$con->query($sql);
-		}
-		if(preg_match("/[0-9]+_hide/",$key)){
-			if($_POST[$key] == 1){
-			$key = strtok($key, '_');
-			$sql = "UPDATE items SET deleted = 0 WHERE id = $key;";
-			$con->query($sql);
-			} else{
-			$key = strtok($key, '_');
-			$sql = "UPDATE items SET deleted = 1 WHERE id = $key;";
-			$con->query($sql);			
-			}
-		}
-		if(preg_match("/[0-9]+_image/",$key)){
-			$key = strtok($key, '_');
-			$sql = "UPDATE items SET image = $value WHERE id = $key;";
-			$con->query($sql);
-		}
 
-	}
+foreach ($_POST as $key => $value)
+{
+    // Update Name
+    if(preg_match("/[0-9]+_name/", $key))
+    {
+        if($value != '')
+        {
+            $id = strtok($key, '_');
+            $value = mysqli_real_escape_string($con, htmlspecialchars($value));
+
+            $sql = "UPDATE items 
+                    SET name='$value' 
+                    WHERE id=$id";
+
+            $con->query($sql);
+        }
+    }
+
+    // Update Price
+    if(preg_match("/[0-9]+_price/", $key))
+    {
+        $id = strtok($key, '_');
+
+        $sql = "UPDATE items 
+                SET price='$value' 
+                WHERE id=$id";
+
+        $con->query($sql);
+    }
+
+    // Update Stock
+    if(preg_match("/[0-9]+_stock/", $key))
+    {
+        $id = strtok($key, '_');
+
+        $sql = "UPDATE items 
+                SET stock='$value' 
+                WHERE id=$id";
+
+        $con->query($sql);
+    }
+
+    // Hide / Show Item
+    if(preg_match("/[0-9]+_hide/", $key))
+    {
+        $id = strtok($key, '_');
+
+        if($_POST[$key] == 1)
+        {
+            $sql = "UPDATE items 
+                    SET deleted=0 
+                    WHERE id=$id";
+        }
+        else
+        {
+            $sql = "UPDATE items 
+                    SET deleted=1 
+                    WHERE id=$id";
+        }
+
+        $con->query($sql);
+    }
+}
+
+
+// IMAGE UPLOAD
+foreach ($_FILES as $key => $file)
+{
+    if(preg_match("/[0-9]+_image/", $key))
+    {
+        $id = strtok($key, '_');
+
+        if(!empty($file['tmp_name']))
+        {
+            $image = addslashes(file_get_contents($file['tmp_name']));
+
+            $sql = "UPDATE items 
+                    SET image='$image' 
+                    WHERE id=$id";
+
+            $con->query($sql);
+        }
+    }
+}
+
 header("location: ../admin-page.php");
 ?>
